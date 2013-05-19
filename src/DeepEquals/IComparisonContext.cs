2e88deaf786798683430c454dccebb1cedaf9a37
James Foster <james.foster@thap.co.uk>
2013-05-19 20:15:19 +0100

    Move ComparisonContext overloads to extension methods

--- src/DeepEquals/IComparisonContext.cs
+++ src/DeepEquals/IComparisonContext.cs
@@ -8,11 +8,8 @@
 		string Breadcrumb { get; }
 
 		void AddDifference(Difference difference);
-		void AddDifference(object value1, object value2);
-		void AddDifference(object value1, object value2, string childProperty);
 
 		IComparisonContext VisitingProperty(string propertyName);
-		IComparisonContext VisitingIndex(string index);
-		IComparisonContext VisitingIndex(int index);
+		IComparisonContext VisitingIndex(object index);
 	}
 }
