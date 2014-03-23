4db99f57321b030203c3b274f3d93a589a0256cc
James Foster <james.mdb@gmail.com>
2014-03-23 23:01:56 +0000

    Encapsulate the algorithm for comparing complex objects

---
+++ src/DeepEqual/ComplexObjectComparer.cs
@@ -0,0 +1,149 @@
+﻿namespace DeepEqual
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	public class ComplexObjectComparer
+	{
+		private readonly IComparison inner;
+		private readonly bool ignoreUnmatchedProperties;
+		private readonly IDictionary<Type, List<string>> ignoredProperties;
+
+		private readonly List<ComparisonResult> results;
+		private Type sourceType;
+		private Type destinationType;
+		private List<PropertyPair> propertyMap;
+		private PropertyPair currentPair;
+
+		public ComplexObjectComparer(IComparison inner, bool ignoreUnmatchedProperties, IDictionary<Type, List<string>> ignoredProperties)
+		{
+			this.inner = inner;
+			this.ignoreUnmatchedProperties = ignoreUnmatchedProperties;
+			this.ignoredProperties = ignoredProperties;
+			results = new List<ComparisonResult>();
+		}
+
+		public ComparisonResult CompareObjects(IComparisonContext context, object source, object destination)
+		{
+			sourceType = source.GetType();
+			destinationType = destination.GetType();
+
+			PreparePropertyInfo(source, destination);
+
+
+			foreach (var pair in propertyMap)
+			{
+				currentPair = pair;
+				var sourceValue = new Lazy<object>(() => currentPair.Source.Read(source));
+				var destinationValue = new Lazy<object>(() => currentPair.Destination.Read(destination));
+
+				if (IsPropertyIgnored())
+				{
+					continue;
+				}
+
+				if (HandleMissingValues(context, destinationValue, sourceValue))
+				{
+					continue;
+				}
+
+				var innerContext = context.VisitingProperty(currentPair.Name);
+				results.Add(inner.Compare(innerContext, sourceValue.Value, destinationValue.Value));
+			}
+
+			return results.ToResult();
+		}
+
+		private bool HandleMissingValues(IComparisonContext context, Lazy<object> destinationValue, Lazy<object> sourceValue)
+		{
+			if (SourceAndDestinationPresent())
+			{
+				return false;
+			}
+
+			if (ignoreUnmatchedProperties)
+			{
+				return true;
+			}
+
+			if (currentPair.Source == null)
+			{
+				context.AddDifference("(missing)", destinationValue.Value, currentPair.Name);
+				results.Add(ComparisonResult.Fail);
+			}
+
+			if (currentPair.Destination == null)
+			{
+				context.AddDifference(sourceValue.Value, "(missing)", currentPair.Name);
+				results.Add(ComparisonResult.Fail);
+			}
+
+			return true;
+		}
+
+		private bool SourceAndDestinationPresent()
+		{
+			return currentPair.Source != null && currentPair.Destination != null;
+		}
+
+		private void PreparePropertyInfo(object source, object destination)
+		{
+			var sourceProperties = ReflectionCache.GetProperties(source);
+			var destinationProperties = ReflectionCache.GetProperties(destination).ToDictionary(x => x.Name);
+
+			propertyMap = new List<PropertyPair>();
+
+			foreach (var property in sourceProperties)
+			{
+				var name = property.Name;
+
+				if (destinationProperties.ContainsKey(name))
+				{
+					propertyMap.Add(new PropertyPair(property, destinationProperties[name], name));
+					destinationProperties.Remove(name);
+				}
+				else
+				{
+					propertyMap.Add(new PropertyPair(property, null, name));
+				}
+			}
+
+			foreach (var property in destinationProperties.Values)
+			{
+				propertyMap.Add(new PropertyPair(null, property, property.Name));
+			}
+		}
+
+		private bool IsPropertyIgnored()
+		{
+			var ignored = GetIgnoredPropertiesForTypes(sourceType, destinationType);
+
+			return ignored.Contains(currentPair.Name);
+		}
+
+		private List<string> GetIgnoredPropertiesForTypes(Type type1, Type type2)
+		{
+			var seed = Enumerable.Empty<string>();
+
+			return ignoredProperties
+				.Where(pair => pair.Key.IsAssignableFrom(type1) || pair.Key.IsAssignableFrom(type2))
+				.Aggregate(seed, (current, pair) => current.Union(pair.Value))
+				.ToList();
+		}
+
+		private class PropertyPair
+		{
+			public PropertyPair(ReflectionCache.PropertyReader source, ReflectionCache.PropertyReader destination, string name)
+			{
+				Source = source;
+				Destination = destination;
+				Name = name;
+			}
+
+			public ReflectionCache.PropertyReader Source { get; private set; }
+			public ReflectionCache.PropertyReader Destination { get; private set; }
+			public string Name { get; private set; }
+		}
+	}
+}
--- src/DeepEqual/ComplexObjectComparison.cs
+++ src/DeepEqual/ComplexObjectComparison.cs
@@ -10,6 +10,7 @@
 		public IComparison Inner { get; set; }
 
 		public bool IgnoreUnmatchedProperties { get; set; }
+
 		public IDictionary<Type, List<string>> IgnoredProperties { get; set; }
 
 		public ComplexObjectComparison(IComparison inner)
@@ -25,61 +26,9 @@
 
 		public ComparisonResult Compare(IComparisonContext context, object value1, object value2)
 		{
-			var type1 = value1.GetType();
-			var type2 = value2.GetType();
+			var comparer = new ComplexObjectComparer(Inner, IgnoreUnmatchedProperties, IgnoredProperties);
 
-			var props1 = ReflectionCache.GetProperties(value1);
-			var props2 = ReflectionCache.GetProperties(value2).ToDictionary(p => p.Name);
-
-			var ignored = GetIgnoredPropertiesForTypes(type1, type2);
-
-			var results = new List<ComparisonResult>();
-
-			foreach (var propertyInfo1 in props1)
-			{
-				if (ignored.Contains(propertyInfo1.Name))
-				{
-					props2.Remove(propertyInfo1.Name);
-					continue;
-				}
-
-				var propValue1 = propertyInfo1.Read(value1);
-
-				if (!props2.ContainsKey(propertyInfo1.Name))
-				{
-					if (!IgnoreUnmatchedProperties)
-					{
-						context.AddDifference(propValue1, "(missing)", propertyInfo1.Name);
-						results.Add(ComparisonResult.Fail);
-					}
-					continue;
-				}
-
-				var propertyInfo2 = props2[propertyInfo1.Name];
-				var propValue2 = propertyInfo2.Read(value2);
-
-				var innerContext = context.VisitingProperty(propertyInfo1.Name);
-				results.Add(Inner.Compare(innerContext, propValue1, propValue2));
-
-				props2.Remove(propertyInfo1.Name);
-			}
-
-			if (!IgnoreUnmatchedProperties && props2.Count > 0)
-			{
-				foreach (var p in props2)
-				{
-					if (ignored.Contains(p.Key))
-					{
-						continue;
-					}
-
-					var v = p.Value.Read(value2);
-					context.AddDifference("(missing)", v, p.Key);
-					results.Add(ComparisonResult.Fail);
-				}
-			}
-
-			return results.ToResult();
+			return comparer.CompareObjects(context, value1, value2);
 		}
 
 		public void IgnoreProperty<T>(Expression<Func<T, object>> property)
@@ -111,16 +60,6 @@
 			}
 
 			IgnoredProperties[type].Add(propertyName);
-		}
-
-		private List<string> GetIgnoredPropertiesForTypes(Type type1, Type type2)
-		{
-			var seed = new List<string>().AsEnumerable();
-
-			return IgnoredProperties
-				.Where(pair => pair.Key.IsAssignableFrom(type1) || pair.Key.IsAssignableFrom(type2))
-				.Aggregate(seed, (current, pair) => current.Union(pair.Value))
-				.ToList();
 		}
 	}
 }
