2d73cd368cc79729a82981c21e7349572158f49c
James Foster <james.mdb@gmail.com>
2014-09-22 17:32:09 +0100

    Include public fields when comparing complex objects

---
+++ src/DeepEqual.Test/Bug/ComparingTypesWithPublicFields.cs
@@ -0,0 +1,35 @@
+﻿namespace DeepEqual.Test.Bug
+{
+	using DeepEqual.Syntax;
+
+	using Shouldly;
+
+	using Xunit;
+
+	public class ComparingTypesWithPublicFields
+	{
+		[Fact]
+		public static void TestIgnoreByType()
+		{
+			var expected = new Data
+				{
+					Id = 1,
+					Name = "Joe"
+				};
+
+			var actual = new Data
+				{
+					Id = 2,
+					Name = "Joe"
+				};
+
+			actual.IsDeepEqual(expected).ShouldBe(false);
+		}
+
+		public class Data
+		{
+			public int Id;
+			public string Name { get; set; }
+		}
+	}
+}
