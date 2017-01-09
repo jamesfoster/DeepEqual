74ae5078df094b0566420b44c57374c5fdbb6455
Brian Drupieski <brian.drupieski@gmail.com>
2017-01-09 16:45:19 -0500

    add DeepEqual.snk and sign assemblies

--- src/DeepEqual/Properties/AssemblyInfo.cs
+++ src/DeepEqual/Properties/AssemblyInfo.cs
@@ -21,4 +21,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.5.1.0")]
 [assembly: AssemblyFileVersion("1.5.1.0")]
 
-[assembly: InternalsVisibleTo("DeepEqual.Test")]
+//http://blog.tylerholmes.com/2008/04/unit-tests-and-internalsvisibleto.html
+[assembly: InternalsVisibleTo("DeepEqual.Test,PublicKey=" +
+"00240000048000009400000006020000002400005253413100040000010001001fe9cb2ded5917" +
+"6088ef825aecda1cbd211fa058786a5c1e77b1f2e217f7ec1234a693b33f3f449b5d03f0027941" +
+"e7e263683de7ae504fbc8f734ab935bb6508249b47beda81ec14e7eb96e2f92f4af67779fc6950" +
+"75cfb38b925820d82e9462d63c3708c453d8d7faf605d328d8ca38e6bd22dac576525808695e13" +
+"4f6cafcc")]
