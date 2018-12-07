8544039ef51c8fb299fd25b8beffc2d246b729cf
James Foster <james.mdb@gmail.com>
2018-12-07 03:00:31 +0000

    Run the tests as part of package.bat

--- package.bat
+++ package.bat
@@ -1,6 +1,13 @@
 @echo off
 
 echo.
+echo === TEST ===
+echo.
+dotnet test  --configuration Release src/DeepEqual.Test/DeepEqual.Test.csproj
+
+echo.
+echo === PACKAGE ===
+echo.
 dotnet pack --include-symbols --include-source --configuration Release --output ..\.. src\DeepEqual\DeepEqual.csproj
 
 echo.
