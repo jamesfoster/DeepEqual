3afd9c3a1ee7e36d6383887f7c1fb9315301060d
James Foster <james.foster@existec.com>
2022-08-20 12:30:09 +0100

    Add readme

---
+++ src/DeepEqual.System.Text.Json/ComparisonBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace DeepEqual.SystemTextJson;
+
+public static class ComparisonBuilderExtensions
+{
+    public static IComparisonBuilder<T> UseSystemTextJson<T>(this IComparisonBuilder<T> builder)
+        where T : IComparisonBuilder<T>
+    {
+        return builder
+            .WithCustomComparison(new JsonDocumentComparison())
+            .WithCustomComparison(new JsonElementComparison());
+    }
+
+    /// <summary>
+    /// Call ComparisonBuilder.Reset to remove all global customizations
+    /// </summary>
+    public static void GloballyUseSystemTextJson()
+    {
+        var getter = ComparisonBuilder.Get;
+        ComparisonBuilder.Get = () => getter().UseSystemTextJson();
+    }
+}
