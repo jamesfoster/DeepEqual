d8723bc73f1d563ecd91a4d90f792846c1a1b0d9
James Foster <james.mdb@gmail.com>
2022-08-20 13:07:22 +0100

    Add steps for packaging Json extension. Use snupkg format for symbols.

--- package.bat
+++ package.bat
@@ -8,7 +8,21 @@ dotnet test  --configuration Release src/DeepEqual.Test/DeepEqual.Test.csproj
 echo.
 echo === PACKAGE ===
 echo.
-dotnet pack --include-symbols --include-source --configuration Release --output ..\.. src\DeepEqual\DeepEqual.csproj
+dotnet pack ^
+  --include-symbols ^
+	 -p:SymbolPackageFormat=snupkg ^
+	--include-source ^
+	--configuration Release ^
+	--output . ^
+	src\DeepEqual\DeepEqual.csproj
+
+dotnet pack ^
+  --include-symbols ^
+	 -p:SymbolPackageFormat=snupkg ^
+	--include-source ^
+	--configuration Release ^
+	--output . ^
+	src\DeepEqual.System.Text.Json\DeepEqual.System.Text.Json.csproj
 
 echo.
 pause
